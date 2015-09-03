e1401fe77fa44b42079017c5aeead406c2beb79e
rbpotter <robertbrucepotter@gmail.com>
2015-09-03 09:30:06 -0500

    Try and commit updated API names again. Git was not happy

---
+++ app/code/Signifyd/Connect/Lib/Models/WebhookResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Signifyd\Connect\Lib\SDK\Models;
+
+use Signifyd\Connect\Lib\SDK\Core\SignifydModel;
+
+/**
+ * Class WebhookResponse
+ * @package Signifyd\Connect\Lib\SDK\Models
+ */
+class WebhookResponse extends SignifydModel
+{
+    public $investigationId;
+    public $analysis;
+}
