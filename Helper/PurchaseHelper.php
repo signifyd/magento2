d6dbb835a001daf1e1ceaaabd752dec64b55f34b
Ébano Penha Andrello Lopes <ebano@onbi.com.br>
2017-06-30 03:14:37 -0300

    Ip detection using regular expressions on filterIP method for better IPv4 and IPv6 detection

--- Helper/PurchaseHelper.php
+++ Helper/PurchaseHelper.php
@@ -112,20 +112,14 @@ class PurchaseHelper
     protected function filterIp($ipString)
     {
         $matches = array();
-        $ipArray = explode(',', $ipString);
-        $ip = $ipArray[0];
 
-        // Uses format IPv4
-        if (preg_match('/[0-9]{1,3}(?:\.[0-9]{1,3}){3}/', $ip, $matches)) {
-            return current($matches);
-        }
+        $pattern = '(([0-9]{1,3}(?:\.[0-9]{1,3}){3})|([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))';
 
-        // Uses format IPv6
-        if (preg_match('/[a-f0-9]{0,4}(?:\:[a-f0-9]{0,4}){2,7}/', strtolower($ip), $matches)) {
-            return current($matches);
-        }
+        preg_match_all($pattern, $ipString, $matches);
 
-        return preg_replace('/[^0-9a-zA-Z:\.]/', '', strtok(str_replace($ip, ',', "\n"), "\n"));
+        if (is_array($matches)) return $matches[0][0];
+
+        return null;
     }
 
     /**
