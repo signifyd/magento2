066d5ea5cc0a7d28cd5b2d93b852573b00c3a4ba
Ébano Penha Andrello Lopes <ebano@onbi.com.br>
2018-03-07 17:00:26 -0300

    MAG1-202: built in conflict messages - Changes behavior of warning and prevent savings from "enable" configuration

--- Model/System/Config/Backend/Enabled.php
+++ Model/System/Config/Backend/Enabled.php
@@ -6,11 +6,39 @@
 namespace Signifyd\Connect\Model\System\Config\Backend;
 
 use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Message\ManagerInterface;
 
 class Enabled extends \Magento\Framework\App\Config\Value
 {
+    protected $messageManager;
 
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        ManagerInterface $messageManager,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->messageManager = $messageManager;
+
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * @return $this
+     */
     public function beforeSave()
     {
         if ($this->getValue() == 0) {
@@ -32,8 +60,12 @@ class Enabled extends \Magento\Framework\App\Config\Value
         $isAnotherEnabled = $this->_config->getValue($path == $signifydPath ? $builtinPath : $signifydPath, 'store');
 
         if ($isAnotherEnabled) {
-            $this->setValue(0);
-            throw new LocalizedException(__('WARNING: Another instance of Signifyd is currently enabled. Please disable the other running instance to avoid conflicts.'));
+            $currentConfig = $this->_config->getValue($builtinPath);
+            if ($currentConfig == 0) {
+                $this->setValue(0);
+            }
+
+            $this->messageManager->addError(__('WARNING: Another instance of Signifyd is currently enabled. Please disable the other running instance to avoid conflicts.'));
         }
         return $this;
     }
