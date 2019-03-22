c50129be2d4be9e8683d1909e2f8b21babef98bd
Ébano Penha Andrello Lopes <ebano.lopes@osf-commerce.com>
2019-03-22 01:20:40 -0300

    3.2.1: MAG-386: Minor changes on settings texts

--- Model/System/Config/Source/Options/Loglevel.php
+++ Model/System/Config/Source/Options/Loglevel.php
@@ -12,9 +12,9 @@ class Loglevel implements \Magento\Framework\Option\ArrayInterface
     public function toOptionArray()
     {
         return [
-            ['value' => 1, 'label' => __('Yes > Info')],
-            ['value' => 0, 'label' => __('No > None')],
-            ['value' => 2, 'label' => __('Debug > Debug')]
+            ['value' => 1, 'label' => __('Info')],
+            ['value' => 0, 'label' => __('None')],
+            ['value' => 2, 'label' => __('Debug')]
         ];
     }
 
@@ -25,6 +25,6 @@ class Loglevel implements \Magento\Framework\Option\ArrayInterface
      */
     public function toArray()
     {
-        return [1 => __('Yes > Info'), 0 => __('No > None'), 2 => __('Debug > Debug')];
+        return [1 => __('Info'), 0 => __('None'), 2 => __('Debug')];
     }
 }
