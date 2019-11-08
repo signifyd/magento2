a745b21b2f5d519498053e61b7dc1dc5601c1b75
Ebano Lopes <ebano@onbi.com.br>
2019-11-08 14:07:52 +0000

    3.5.0: MAG-494: Collect data for Stripe

---
+++ Model/Payment/Stripe/Payments/ExpMonthMapper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Signifyd\Connect\Model\Payment\Stripe\Payments;
+
+use Signifyd\Connect\Model\Payment\Base\ExpMonthMapper as Base_ExpMonthMapper;
+
+class ExpMonthMapper extends Base_ExpMonthMapper
+{
+    use MapperTrait;
+
+    protected $allowedMethods = array('stripe_payments');
+
+    /**
+     * Gets expiry month
+     *
+     * @param \Magento\Sales\Model\Order $order
+     * @return null|string
+     */
+    public function getPaymentData(\Magento\Sales\Model\Order $order)
+    {
+        $charge = $this->getCharge($order);
+
+        if (is_object($charge) == false) {
+            $expMonth = null;
+        }
+
+        if (isset($charge->payment_method_details) &&
+            isset($charge->payment_method_details->card) &&
+            isset($charge->payment_method_details->card->exp_month)
+        ) {
+            $expMonth = $charge->payment_method_details->card->exp_month;
+        } else {
+            $expMonth = null;
+        }
+
+        $this->logger->debug('Expiry month found on payment mapper: ' . (empty($expMonth) ? 'false' : $expMonth), array('entity' => $order));
+
+        if (empty($expMonth)) {
+            $expMonth = parent::getPaymentData($order);
+        }
+
+        return $expMonth;
+    }
+}
---
+++ Model/Payment/Stripe/Payments/ExpYearMapper.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Signifyd\Connect\Model\Payment\Stripe\Payments;
+
+use Signifyd\Connect\Model\Payment\Base\ExpYearMapper as Base_ExpYearMapper;
+
+class ExpYearMapper extends Base_ExpYearMapper
+{
+    use MapperTrait;
+
+    protected $allowedMethods = array('stripe_payments');
+
+    /**
+     * Gets expiry year
+     *
+     * @param \Magento\Sales\Model\Order $order
+     * @return null|string
+     */
+    public function getPaymentData(\Magento\Sales\Model\Order $order)
+    {
+        $charge = $this->getCharge($order);
+
+        if (is_object($charge) == false) {
+            $expYear = null;
+        }
+
+        if (isset($charge->payment_method_details) &&
+            isset($charge->payment_method_details->card) &&
+            isset($charge->payment_method_details->card->exp_year)
+        ) {
+            $expYear = $charge->payment_method_details->card->exp_year;
+            $expYear = substr($expYear, -2);
+        } else {
+            $expYear = null;
+        }
+
+        $this->logger->debug('Expiry year found on payment mapper: ' . (empty($expYear) ? 'false' : $expYear), array('entity' => $order));
+
+        if (empty($expYear)) {
+            $expYear = parent::getPaymentData($order);
+        }
+
+        return $expYear;
+    }
+}
