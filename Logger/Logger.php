e1928384283642b38a2b8fbe083d24db12c4d7f9
Ebano Lopes <ebano@onbi.com.br>
2019-11-13 10:12:37 +0000

    3.5.0: MAG-503: Minor fix Previous file version raised exception on di:compile

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -29,10 +29,10 @@ class Logger extends \Monolog\Logger
      * @param ConfigHelper $configHelper
      */
     public function __construct(
-        ConfigHelper $configHelper,
         $name,
         array $handlers = [],
-        array $processors = []
+        array $processors = [],
+        ConfigHelper $configHelper = null
     ) {
         $this->configHelper = $configHelper;
         $this->log = $this->configHelper->getConfigData('signifyd/logs/log');
