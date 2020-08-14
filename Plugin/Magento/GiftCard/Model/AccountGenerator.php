e9e81c2d30aaa4fdb84bb9ff9218b8d4e954ebb3
Ebano Lopes <ebano@onbi.com.br>
2020-08-14 18:10:35 +0000

    MAG-580: Fix to work with Magento Commerce gift card functionality Magento Commerce gift card duplicates gift card accounts when Signifyd extension saves the order on checkout process But Signifyd extension does need to act on checkout process and also needs to save the order This fix acts on gift card functionality, preventing it from generate more accounts than purchased

---
+++ Plugin/Magento/GiftCard/Model/AccountGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Signifyd\Connect\Plugin\Magento\GiftCard\Model;
+
+use Magento\GiftCard\Model\AccountGenerator as MagentoAccountGenerator;
+use Magento\Sales\Model\Order\Item as OrderItem;
+
+class AccountGenerator
+{
+    /**
+     * Do not allow to generate more gift cards than purchased
+     *
+     * @param MagentoAccountGenerator $subject
+     * @param OrderItem $orderItem
+     * @param int $qty
+     * @param array $options
+     * @return array
+     */
+    public function beforeGenerate(MagentoAccountGenerator $subject, OrderItem $orderItem, int $qty, array $options)
+    {
+        $giftCardCreatedCodes = $orderItem->getProductOptionByCode('giftcard_created_codes');
+        $orderItemQty = $orderItem->getQtyOrdered() - $orderItem->getQtyCanceled() - $orderItem->getQtyRefunded();
+
+        if ($qty > $orderItemQty - count($giftCardCreatedCodes)) {
+            $qty = $orderItemQty - count($giftCardCreatedCodes);
+        }
+
+        return [$orderItem, $qty, $options];
+    }
+}
