4195152f38c05dd6ec66910435bfa2f48924e4f5
Ébano Lopes <ebano@onbi.com.br>
2023-01-27 13:54:21 +0000

    MAG-809: Some 3DS data is not being sent to Signifyd

--- Plugin/Adyen/Payment/Gateway/Request/CheckoutDataBuilderSca.php
+++ Plugin/Adyen/Payment/Gateway/Request/CheckoutDataBuilderSca.php
@@ -137,13 +137,12 @@ class CheckoutDataBuilderSca
                 $this->logger->info("Signifyd's recommendation is to request exemption");
 
                 $placement = $scaEvaluation->exemptionDetails->placement;
+                $scaExemption = 'tra';
 
                 if ($placement === 'AUTHENTICATION') {
-                    $executeThreeD = 'True';
-                    $scaExemption = 'tra';
+                    $executeThreeD = 'always';
                 } elseif ($placement === 'AUTHORIZATION') {
-                    $executeThreeD = 'False';
-                    $scaExemption = 'tra';
+                    $executeThreeD = 'never';
                 }
                 break;
 
@@ -161,13 +160,13 @@ class CheckoutDataBuilderSca
             case 'SOFT_DECLINE':
                 $this->logger->info("Processor responds back with a soft decline");
 
-                $executeThreeD = 'True';
+                $executeThreeD = 'always';
                 $scaExemption = '';
                 break;
         }
 
         if (isset($executeThreeD) && isset($scaExemption)) {
-            $request['body']['additionalData']['executeThreeD'] = $executeThreeD;
+            $request['body']['authenticationData']['attemptAuthentication'] = $executeThreeD;
             $request['body']['additionalData']['scaExemption'] = $scaExemption;
         }
 
