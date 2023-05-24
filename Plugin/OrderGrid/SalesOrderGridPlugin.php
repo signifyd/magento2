e039fe9e12d9ea629e175dcd682977b25f6183bf
Ébano Lopes <ebano@onbi.com.br>
2023-05-24 14:28:44 +0000

    MAG-24_5.2.0: Fixed order grid export issues (branch 5.2.0).

---
+++ Plugin/OrderGrid/SalesOrderGridPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Signifyd\Connect\Plugin\OrderGrid;
+
+class SalesOrderGridPlugin
+{
+    public function aroundGetSelect(
+        \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult $subject,
+        \Closure                                                              $proceed
+    )
+    {
+        $select = $proceed();
+        $connection = $subject->getResource()->getConnection();
+
+        if ($subject->getMainTable() === $connection->getTableName('sales_order_grid')) {
+            $parts = $select->getPart(\Magento\Framework\DB\Select::FROM);
+            if (!isset($parts['signifyd_connect_case'])) {
+                $select->joinLeft(
+                    ['signifyd_connect_case' => $subject->getTable('signifyd_connect_case')],
+                    'main_table.entity_id = signifyd_connect_case.order_id',
+                    ['signifyd_score' => 'signifyd_connect_case.score', 'signifyd_guarantee' => 'signifyd_connect_case.guarantee', 'checkpoint_action_reason' => 'signifyd_connect_case.checkpoint_action_reason']
+                );
+            }
+        }
+        return $select;
+    }
+}
