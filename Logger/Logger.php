01d7d230968f238494f726a64748e6bd0534f138
Ébano Lopes <ebano@onbi.com.br>
2023-07-05 14:19:18 +0000

    5.4.0: compatibility minor fix.

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -7,58 +7,6 @@
 
 namespace Signifyd\Connect\Logger;
 
-use Signifyd\Connect\Helper\ConfigHelper;
-
 class Logger extends \Monolog\Logger
 {
-    /**
-     * @var bool
-     */
-    protected $log;
-
-    /**
-     * @var ConfigHelper
-     */
-    protected $configHelper;
-
-    /**
-     * Logger constructor.
-     * @param string $name
-     * @param array $handlers
-     * @param array $processors
-     * @param ConfigHelper $configHelper
-     */
-    public function __construct(
-        $name,
-        ConfigHelper $configHelper,
-        array $handlers = [],
-        array $processors = []
-    ) {
-        $this->configHelper = $configHelper;
-        $this->log = $this->configHelper->getConfigData('signifyd/logs/log');
-
-        return parent::__construct($name, $handlers, $processors);
-    }
-
-    /**
-     * @param int $level
-     * @param string $message
-     * @param array $context
-     * @return bool
-     */
-    public function addRecord(int $level, $message, array $context = [], $datetime = null): bool
-    {
-        if (isset($context['entity'])) {
-            $log = $this->configHelper->getConfigData('signifyd/logs/log', $context['entity']);
-            unset($context['entity']);
-        } else {
-            $log = $this->log;
-        }
-
-        if ($log == false) {
-            return false;
-        }
-
-        return parent::addRecord($level, $message, $context, $datetime);
-    }
 }
