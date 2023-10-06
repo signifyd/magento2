1592a30186d7e69554dfe0f7880773f81d67e944
Cristian Balaj <cristian.balaj@osf.digital>
2023-10-06 16:18:14 +0300

    MAG-850 added AVS mapper for AuthorizeNET bt RootWays

---
+++ Model/Payment/RootwaysAuthorizecimOption/AvsEmsCodeMapper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Signifyd\Connect\Model\Payment\RootwaysAuthorizecimOption;
+
+use Signifyd\Connect\Model\Payment\Base\AvsEmsCodeMapper as Base_AvsEmsCodeMapper;
+
+class AvsEmsCodeMapper extends Base_AvsEmsCodeMapper
+{
+    protected $allowedMethods = ['rootways_authorizecim_option'];
+
+    /**
+     * Gets payment AVS verification code.
+     *
+     * @param \Magento\Sales\Model\Order $order
+     * @return null|string
+     */
+    public function getPaymentData(\Magento\Sales\Model\Order $order)
+    {
+        $additionalInfo = $order->getPayment()->getAdditionalInformation();
+
+        if (empty($additionalInfo['avs_response_code']) == false) {
+            $avsStatus = $additionalInfo['avs_response_code'];
+            if ($avsStatus == 'B') {
+                $avsStatus = 'U';
+            }
+        }
+
+        if ($this->validate($avsStatus) == false) {
+            $avsStatus = null;
+        }
+
+        $message = 'AVS found on payment mapper: ' . (empty($avsStatus) ? 'false' : $avsStatus);
+        $this->logger->debug($message, ['entity' => $order]);
+
+        if (empty($avsStatus)) {
+            $avsStatus = parent::getPaymentData($order);
+        }
+        return $avsStatus;
+    }
+}
