3359e8b464076d50e4b71b5b1de246b504268423
Ébano Lopes <ebano@onbi.com.br>
2024-01-16 15:36:09 +0000

    MS-266: MIDO MX: Card details not received in 3DS verification failures

--- Plugin/Adyen/Payment/Helper/PaymentResponseHandler.php
+++ Plugin/Adyen/Payment/Helper/PaymentResponseHandler.php
@@ -4,9 +4,16 @@ namespace Signifyd\Connect\Plugin\Adyen\Payment\Helper;
 
 use Magento\Framework\Serialize\Serializer\Json as JsonSerializer;
 use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order;
+use Signifyd\Connect\Helper\OrderHelper;
+use Signifyd\Connect\Logger\Logger;
+use Signifyd\Connect\Model\Casedata;
+use Signifyd\Connect\Model\ResourceModel\Casedata as CasedataResourceModel;
+use Signifyd\Connect\Model\ResourceModel\Order as SignifydOrderResourceModel;
 use Signifyd\Connect\Model\ThreeDsIntegration;
 use Adyen\Payment\Helper\PaymentResponseHandler as AdyenPaymentResponseHandler;
 use Magento\Framework\App\Request\Http as HttpRequest;
+use Signifyd\Connect\Model\CasedataFactory;
 
 class PaymentResponseHandler
 {
@@ -31,21 +38,61 @@ class PaymentResponseHandler
     private $orderRepository;
 
     /**
+     * @var SignifydOrderResourceModel
+     */
+    protected $signifydOrderResourceModel;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * @var OrderHelper
+     */
+    protected $orderHelper;
+
+    /**
+     * @var CasedataFactory
+     */
+    protected $casedataFactory;
+
+    /**
+     * @var CasedataResourceModel
+     */
+    protected $casedataResourceModel;
+
+    /**
      * @param ThreeDsIntegration $threeDsIntegration
      * @param HttpRequest $httpRequest
      * @param JsonSerializer $jsonSerializer
      * @param OrderRepositoryInterface $orderRepository
+     * @param SignifydOrderResourceModel $signifydOrderResourceModel
+     * @param Logger $logger
+     * @param CasedataFactory $casedataFactory
+     * @param CasedataResourceModel $casedataResourceModel
+     * @param OrderHelper $orderHelper
      */
     public function __construct(
         ThreeDsIntegration $threeDsIntegration,
         HttpRequest $httpRequest,
         JsonSerializer $jsonSerializer,
-        OrderRepositoryInterface $orderRepository
+        OrderRepositoryInterface $orderRepository,
+        SignifydOrderResourceModel $signifydOrderResourceModel,
+        Logger $logger,
+        CasedataFactory $casedataFactory,
+        CasedataResourceModel $casedataResourceModel,
+        OrderHelper $orderHelper
     ) {
         $this->threeDsIntegration = $threeDsIntegration;
         $this->httpRequest = $httpRequest;
         $this->jsonSerializer = $jsonSerializer;
         $this->orderRepository = $orderRepository;
+        $this->signifydOrderResourceModel = $signifydOrderResourceModel;
+        $this->logger = $logger;
+        $this->casedataFactory = $casedataFactory;
+        $this->casedataResourceModel = $casedataResourceModel;
+        $this->orderHelper = $orderHelper;
     }
 
     public function beforeFormatPaymentResponse(
@@ -55,17 +102,17 @@ class PaymentResponseHandler
         $additionalData = null
     ) {
         if (isset($additionalData) === false || is_array($additionalData) === false) {
-            return;
+            return [$resultCode, $action, $additionalData];
         }
 
         if (isset($additionalData['threeDAuthenticated']) === false) {
-            return;
+            return [$resultCode, $action, $additionalData];
         }
 
         if ($additionalData['threeDAuthenticated'] === 'false' &&
             isset($additionalData['scaExemptionRequested']) === false
         ) {
-            return;
+            return [$resultCode, $action, $additionalData];
         }
 
         $content = $this->httpRequest->getContent();
@@ -77,12 +124,12 @@ class PaymentResponseHandler
             if (isset($payload['orderId'])) {
                 $orderId = $payload['orderId'];
             } else {
-                return;
+                return [$resultCode, $action, $additionalData];
             }
         } elseif (isset($contentArray['orderId'])) {
             $orderId = $contentArray['orderId'];
         } else {
-            return;
+            return [$resultCode, $action, $additionalData];
         }
 
         $order = $this->orderRepository->get($orderId);
@@ -161,5 +208,74 @@ class PaymentResponseHandler
         $this->threeDsIntegration->setThreeDsData($threeDsData, $quoteId);
 
         return [$resultCode, $action, $additionalData];
+    }
+
+    public function beforeHandlePaymentResponse(
+        AdyenPaymentResponseHandler $subject,
+        $paymentsResponse,
+        $payment,
+        $order = null
+    ) {
+        if (empty($paymentsResponse) || null === $order) {
+            return [$paymentsResponse, $payment, $order];
+        }
+
+        try {
+            if (isset($paymentsResponse['resultCode']) && $paymentsResponse['resultCode'] === $subject::REFUSED) {
+                $orderId = $order->getId();
+                $case = $this->casedataFactory->create();
+                $this->casedataResourceModel->load($case, $orderId, 'order_id');
+
+                if ($case->isEmpty()) {
+                    return [$paymentsResponse, $payment, $order];
+                }
+
+                if ($case->getData('magento_status') === Casedata::ASYNC_WAIT && empty($case->getData('code'))) {
+                    $case->setEntries('async_action', 'delete');
+                    $this->casedataResourceModel->save($case);
+                }
+
+                if ($order->canUnhold()) {
+                    try {
+                        $order->unhold();
+                        $this->signifydOrderResourceModel->save($order);
+                        $this->logger->info(
+                            "Unhold order {$order->getIncrementId()} before adyen tries to cancel",
+                            ['entity' => $order]
+                        );
+
+                        $this->orderHelper->addCommentToStatusHistory(
+                            $order,
+                            "Signifyd: unhold order before adyen tries to cancel"
+                        );
+                    } catch (\Exception $e) {
+                        $this->logger->debug($e->__toString(), ['entity' => $order]);
+
+                        $this->orderHelper->addCommentToStatusHistory(
+                            $order,
+                            "Signifyd: order cannot be unholded, {$e->getMessage()}"
+                        );
+                    }
+                }
+            }
+        } catch (\Exception $ex) {
+            $context = [];
+
+            if (isset($order) && $order instanceof Order) {
+                $context['entity'] = $order;
+            }
+
+            $this->logger->error($ex->getMessage(), $context);
+        } catch (\Error $ex) {
+            $context = [];
+
+            if (isset($order) && $order instanceof Order) {
+                $context['entity'] = $order;
+            }
+
+            $this->logger->error($ex->getMessage(), $context);
+        }
+
+        return [$paymentsResponse, $payment, $order];
     }
 }
