989e4481b06d1ce689bc0817809142c847f5ece9
ebanolopes <ebano@onbi.com.br>
2025-06-20 18:39:25 +0000

    MS-343: Sezzle Orders not being filtered out and not being passed with transactions array

--- Observer/PreAuth.php
+++ Observer/PreAuth.php
@@ -17,6 +17,7 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Signifyd\Connect\Model\Api\CheckoutOrderFactory;
 use Signifyd\Connect\Model\Api\Core\Client;
 use Signifyd\Connect\Model\Casedata;
+use Signifyd\Connect\Model\Registry;
 use Signifyd\Connect\Model\ResourceModel\Casedata as CasedataResourceModel;
 use Signifyd\Connect\Model\CasedataFactory;
 use Magento\Framework\App\Request\Http as RequestHttp;
@@ -107,6 +108,11 @@ class PreAuth implements ObserverInterface
     public $recipient;
 
     /**
+     * @var Registry
+     */
+    public $registry;
+
+    /**
      * PreAuth constructor.
      * @param Logger $logger
      * @param CartRepositoryInterface $quoteRepository
@@ -124,6 +130,7 @@ class PreAuth implements ObserverInterface
      * @param CheckoutOrderFactory $checkoutOrderFactory
      * @param Client $client
      * @param Recipient $recipient
+     * @param Registry $registry
      */
     public function __construct(
         Logger $logger,
@@ -141,7 +148,8 @@ class PreAuth implements ObserverInterface
         ObjectManagerInterface $objectManagerInterface,
         CheckoutOrderFactory $checkoutOrderFactory,
         Client $client,
-        Recipient $recipient
+        Recipient $recipient,
+        Registry $registry
     ) {
         $this->logger = $logger;
         $this->quoteRepository = $quoteRepository;
@@ -159,6 +167,7 @@ class PreAuth implements ObserverInterface
         $this->checkoutOrderFactory = $checkoutOrderFactory;
         $this->client = $client;
         $this->recipient = $recipient;
+        $this->registry = $registry;
     }
 
     public function execute(Observer $observer)
@@ -191,6 +200,10 @@ class PreAuth implements ObserverInterface
                 isset($dataArray['paymentMethod']['method'])
             ) {
                 $paymentMethod = $dataArray['paymentMethod']['method'];
+            }
+
+            if (isset($paymentMethod) === false) {
+                $paymentMethod = $this->registry->getData('paymentMethod');
             }
 
             if (isset($paymentMethod) && $this->configHelper->isPaymentRestricted($paymentMethod)) {
