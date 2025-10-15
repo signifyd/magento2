a2f3a0ed7bbb445e8c110c688080a1beafadee38
ebanolopes <ebano@onbi.com.br>
2025-10-15 22:01:18 +0000

    MAG-971: Add cvc_check to Stripe Async Wait Mapper

--- Observer/Purchase.php
+++ Observer/Purchase.php
@@ -412,17 +412,14 @@ class Purchase implements ObserverInterface
                 $case->setEntries('processed_by_gateway', false);
             }
 
-            $hasAsyncRestriction = $this->getHasAsyncRestriction($paymentMethod);
-
-            if (in_array($paymentMethod, $this->getAsyncPaymentMethodsConfig()) && $hasAsyncRestriction === false) {
-
+            if (in_array($paymentMethod, $this->getAsyncPaymentMethodsConfig())) {
                 /** @var \Signifyd\Connect\Model\Payment\Base\AsyncChecker $asyncCheck */
                 $asyncCheck = $this->paymentVerificationFactory->createPaymentAsyncChecker(
                     $order->getPayment()->getMethod()
                 );
             }
 
-            if (isset($asyncCheck)) {
+            if (isset($asyncCheck) && $asyncCheck($order, $case) === false) {
                 $case->setMagentoStatus(Casedata::ASYNC_WAIT);
 
                 try {
@@ -536,29 +533,6 @@ class Purchase implements ObserverInterface
         $asyncPaymentMethods = array_map('trim', $asyncPaymentMethods);
 
         return $asyncPaymentMethods;
-    }
-
-    /**
-     * Get async payment methods from store configs
-     *
-     * @param mixed $paymentMethod
-     * @return bool
-     */
-    public function getHasAsyncRestriction($paymentMethod)
-    {
-        try {
-            //stripe payments no longer needs to be async in versions 3.4.0 or higher
-            if ($paymentMethod == 'stripe_payments') {
-                $stripeVersion = \StripeIntegration\Payments\Model\Config::$moduleVersion;
-                $process = version_compare($stripeVersion, '3.4.0') >= 0 ? "synchronous" : "asynchronous";
-
-                $this->logger->info("Stripe on version {$stripeVersion} is " . $process);
-
-                return version_compare($stripeVersion, '3.4.0') >= 0;
-            }
-        } catch (\Exception $e) {
-            return false;
-        }
     }
 
     /**
