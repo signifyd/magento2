47d496bb98a0d8e1865be493182e19d02f8a0bf4
ebanolopes <ebano@onbi.com.br>
2025-12-15 14:39:03 +0000

    Resolve Performance and Data Integrity Issues

--- Ui/Component/DataProvider/OrderGridDataProvider.php
+++ Ui/Component/DataProvider/OrderGridDataProvider.php
@@ -2,17 +2,63 @@
 
 namespace Signifyd\Connect\Ui\Component\DataProvider;
 
-use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\ReportingInterface;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+use Magento\Framework\App\RequestInterface;
+use Signifyd\Connect\Model\ResourceModel\Casedata\CollectionFactory as SignifydCollectionFactory;
 
 class OrderGridDataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
 {
+    /**
+     * @var SignifydCollectionFactory
+     */
+    public $signifydCollectionFactory;
+
+    /**
+     * @param SignifydCollectionFactory $signifydCollectionFactory
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param ReportingInterface $reporting
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param RequestInterface $request
+     * @param FilterBuilder $filterBuilder
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        SignifydCollectionFactory $signifydCollectionFactory,
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        ReportingInterface $reporting,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        RequestInterface $request,
+        FilterBuilder $filterBuilder,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->signifydCollectionFactory = $signifydCollectionFactory;
+        parent::__construct(
+            $name,
+            $primaryFieldName,
+            $requestFieldName,
+            $reporting,
+            $searchCriteriaBuilder,
+            $request,
+            $filterBuilder,
+            $meta,
+            $data
+        );
+    }
+
     /**
      * @inheritdoc
      */
     public function addFilter(\Magento\Framework\Api\Filter $filter)
     {
         $field = $filter->getField();
-        
         $signifydFields = [
             'signifyd_score' => 'score',
             'signifyd_guarantee' => 'guarantee',
@@ -23,10 +69,8 @@ class OrderGridDataProvider extends \Magento\Framework\View\Element\UiComponent\
             return parent::addFilter($filter);
         }
 
-        //TODO: REMOVER
         /** @var \Signifyd\Connect\Model\ResourceModel\Casedata\Collection $signifydCollection */
-        $signifydCollection = \Magento\Framework\App\ObjectManager::getInstance()
-            ->get('Signifyd\Connect\Model\ResourceModel\Casedata\CollectionFactory')->create();
+        $signifydCollection = $this->signifydCollectionFactory->create();
 
         $mappedField = $signifydFields[$field];
         $condition = $filter->getConditionType();
